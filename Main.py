ceef9d76d183046bd50aa2129c3a5f0eb0706ea0
Mohammad Huzefa Shaikh <shaikhhuzaifahusain@gmail.com>
2020-11-12 13:41:39 +0530

    Testing git

--- Main.py
+++ Main.py
@@ -4,27 +4,29 @@ import pandas as pd
 from tensorflow.keras.models import model_from_yaml
 from performForecastClassFile import performForecast
 
-
 yaml_file = open('model/rel_model.yaml', 'r')
 loaded_model_yaml = yaml_file.read()
 yaml_file.close()
 lstm = model_from_yaml(loaded_model_yaml)
 # load weights into new model
 lstm.load_weights("model/rel_model.h5")
-print("Loaded model from disk") # Load "model"
+print("Loaded model from disk")  # Load "model"
 print('Model loaded')
 
 st.subheader('Reliance Stock Forecast')
-#@st.cache
+
+
+# @st.cache
 def load_data(user_input):
     prediction = performForecast(int(user_input)).predict(lstm)
     base = datetime.datetime.today()
     date_list = [(base + datetime.timedelta(days=x + 1)).strftime('%m-%d-%Y') for x in range(int(user_input))]
-    DF = pd.DataFrame()
-    DF['Date'] = date_list
-    DF['Date'] = pd.to_datetime(DF['Date'])
-    DF['Future_prices'] = prediction
-    return DF
+    df = pd.DataFrame()
+    df['Date'] = date_list
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Future_prices'] = prediction
+    return df
+
 
 default = 10
 future_days = st.slider('Number of Future Forecast Days', 1, 30, default)
@@ -36,4 +38,3 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Stock closing on each day')
 st.line_chart(data['Future_prices'])
-
